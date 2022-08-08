241b88ba734bff9baebdbf746c92869dfa0bbd50
Maks Apakov <maksapakov>
2022-08-08 19:42:31 +0300

    ver. 0.5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         // инициализация анализаторов для проверки в порядке данного набора анализаторов
@@ -47,6 +45,7 @@ public class Main {
         tests[7] = "Very bad, very neg =(, very ..................";      // SPAM or NEGATIVE_TEXT or TOO_LONG
         TextAnalyzer[][] textAnalyzers = {textAnalyzers1, textAnalyzers2, textAnalyzers3,
                 textAnalyzers4, textAnalyzers5, textAnalyzers6};
+        //noinspection InstantiationOfUtilityClass
         Main testObject = new Main();
         int numberOfAnalyzer; // номер анализатора, указанный в идентификаторе textAnalyzers{№}
         int numberOfTest = 0; // номер теста, который соответствует индексу тестовых комментариев
@@ -56,92 +55,86 @@ public class Main {
             System.out.println(test);
             for (TextAnalyzer[] analyzers : textAnalyzers) {
                 System.out.print(numberOfAnalyzer + ": ");
+                //noinspection AccessStaticViaInstance
                 System.out.println(testObject.checkLabels(analyzers, test));
                 numberOfAnalyzer++;
             }
             numberOfTest++;
         }
-        SpamAnalyzer spamAnalyzer = new SpamAnalyzer(spamKeywords);
-        System.out.println(Arrays.toString(spamAnalyzer.getKeyword()));
-        System.out.println(spamAnalyzer.processText(tests[5]) + " test");
-//        System.out.println(tests[5].regionMatches(true,17,"spam",0,4) + " : " + tests[5]);
+        String[] badWords = {"спам", "отстой"};
+        SpamAnalyzer spamA = new SpamAnalyzer(badWords);
+        NegativeTextAnalyzer negativeTA = new NegativeTextAnalyzer();
+        TooLongTextAnalyzer tooLongTA = new TooLongTextAnalyzer(40);
+        TextAnalyzer[] analyzers = {spamA, negativeTA, tooLongTA};
+        System.out.println(checkLabels(analyzers, "Хороший текст для теста."));
+        System.out.println(checkLabels(analyzers, "Плохой текст для теста, так как это :( ужас."));
+        System.out.println(checkLabels(analyzers, "Плохой текст для теста, полный отстой."));
+        System.out.println(checkLabels(analyzers, "И это все, что вы хотели написать в комментариях. Да ну, вообще..."));
 
 
     }
-    public Label checkLabels(TextAnalyzer[] textAnalyzer, String text) {
 
-        return Label.OK;
-    }
-
-    public static class SpamAnalyzer implements TextAnalyzer {
-        private String[] keywords;
-
-        public SpamAnalyzer(String[] spamKeywords) {
-            this.keywords = spamKeywords.clone();
-        }
-
-        private String[] getKeyword() {
-            return keywords;
-        }
-
-        private Label getLabel() {
-            return Label.SPAM;
-        }
-
-        @Override
-        public Label processText(String text) {
-
-            Label label = getLabel();
-
-            for (String keyword:
-                    keywords) {
-/*
-                if (text.regionMatches(true,0,keyword,0,keyword.length()) == false) {
-                    label = Label.OK;
-                } else {
-                    getLabel();
-                }
-*/
-               label = text.regionMatches(true, 0, keyword, 0, keyword.length()) ?
-                       getLabel() : Label.OK;
-//                System.out.println(text.regionMatches(true,0,keyword,0,keyword.length()) + " : " + keyword + " : " + text);
-            }
-            return label;
+public static Label checkLabels(TextAnalyzer[] textAnalyzer, String text) {
+    TextAnalyzer[] textAnalyzers = textAnalyzer.clone();
+    for (TextAnalyzer textAnalyzer1 :
+            textAnalyzers) {
+        if (textAnalyzer1.processText(text) != Label.OK) {
+            return textAnalyzer1.processText(text);
         }
     }
+    return Label.OK;
+}
 
-    public static class NegativeTextAnalyzer implements TextAnalyzer {
+public static class SpamAnalyzer extends KeywordAnalyzer {
+    private final String[] keywords;
 
-        @Override
-        public Label processText(String text) {
-            return null;
-        }
+    public SpamAnalyzer(String[] spamKeywords) {
+        this.keywords = spamKeywords.clone();
     }
 
-    public static class TooLongTextAnalyzer implements TextAnalyzer {
-        public TooLongTextAnalyzer(int commentMaxLength) {
-        }
 
-        @Override
-        public Label processText(String text) {
-            return null;
-        }
+    public String[] getKeyword() {
+        return keywords;
     }
 
-    public abstract class KeywordAnalyzer implements TextAnalyzer{
-
-        private String str;
-
-        protected abstract Label getLabel();
-        protected abstract String[] getKeyword();
-
-        public Label processText(String text) {
-
-            this.str = text;
-
-
-            return null;
-        }
-
+    public Label getLabel() {
+        return Label.SPAM;
     }
+
+}
+
+public static class NegativeTextAnalyzer extends KeywordAnalyzer {
+    private final String[] keywords = {":(", "=(", ":|"};
+
+    protected String[] getKeyword() {
+        return keywords;
+    }
+
+    protected Label getLabel() {
+        return Label.NEGATIVE_TEXT;
+    }
+}
+
+public static class TooLongTextAnalyzer implements TextAnalyzer {
+    private final int maxLength;
+
+    public TooLongTextAnalyzer(int commentMaxLength) {
+        this.maxLength = commentMaxLength;
+    }
+
+    private Label getLabel() {
+        return Label.TOO_LONG;
+    }
+
+    @Override
+    public Label processText(String text) {
+        if (text.length() >= maxLength) {
+            return getLabel();
+        } else {
+            return Label.OK;
+        }
+    }
+}
+
+
 }
